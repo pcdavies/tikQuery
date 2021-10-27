45998703040466c1840bea99480625efecb78891
Patrick Davies <pcdavies@users.noreply.github.com>
2021-10-26 22:48:16 -0600

    More detail

--- tikQuery.py
+++ tikQuery.py
@@ -7,7 +7,7 @@ verifyFp = ""
 
 api = TikTokApi.get_instance()
 count = 1000
-tiktoks = api.by_username("jdisjustin", count=count)
+tiktoks = api.by_username("jdisjustin", count=count, custom_verifyFp=verifyFp)
 
 # trending = api.by_trending(count=results, custom_verifyFp=verifyFp)
 printHeader = True
@@ -20,15 +20,27 @@ for tiktok in tiktoks:
         totalFollowers = tiktok['authorStats']['followerCount']
         # pp.pprint(tiktoks)
         print('\n\n')
-        print('      Views   Comments      Likes  Shares  Description')
-        print('-----------   --------   -------- -------  ------------------------------------------------------------')
+        print('      Views   Comments    %        Likes     %   Shares      %  Description')
+        print('-----------   -------- -----  ---------- -----   ------   ----  ------------------------------------------------------------')
+
+    playCount = tiktok['stats']['playCount']
+    commentCount = tiktok['stats']['commentCount']
+    diggCount = tiktok['stats']['diggCount']
+    shareCount = tiktok['stats']['shareCount']
+    descr = tiktok['desc']
+
+    percentComments = commentCount / playCount * 100.0
+    percentDiggs = diggCount / playCount * 100.0
+    percentShares = shareCount / playCount * 100.0
 
     # print(f"{tiktok['desc']}")
-    print(f"{tiktok['stats']['playCount']:11,.0f} ", end='')
-    print(f"{tiktok['stats']['commentCount']:10,.0f} ", end='')
-    print(f"{tiktok['stats']['diggCount']:10,.0f} ", end='')
-    print(f"{tiktok['stats']['shareCount']:7,.0f}  ", end='')
-    descr = tiktok['desc']
+    print(f'{playCount:11,.0f} ', end='')
+    print(f"{commentCount:10,.0f} ", end='')
+    print(f"{percentComments:5.2f} ", end=' ')
+    print(f"{diggCount:10,.0f} ", end='')
+    print(f"{percentDiggs:5.1f} ", end=' ')
+    print(f"{shareCount:7,.0f}  ", end='')
+    print(f"{percentShares:5.2f} ", end=' ')
     if len(descr) > 60:
         descr = descr[0:59]
     print(f"{descr}")
